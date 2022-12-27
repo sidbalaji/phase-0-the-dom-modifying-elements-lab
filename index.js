34302f1e6625a6fcffecd3f83dd394be7b852f31
Siddarth Balaji <sid.balaji@berkeley.edu>
2022-12-27 12:56:21 -0400

    commit tree fea6dfe247280d3a1153c347d4a90b813f7255a6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const element = document.getElementById("main");
+element.remove()
+
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+document.body.append(newHeader)
